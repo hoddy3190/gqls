b457a348fc6502220877d12262ecccb80e611620
Hodaka Suzuki <altitude3190@gmail.com>
2024-08-18 10:47:46 +0900

    feat: parse strictly

--- src/media-type.ts
+++ src/media-type.ts
@@ -1,48 +1,88 @@
 import { assert } from "node:console";
 
-export const JSON = "application/json";
-export const URL_ENCODED = "application/x-www-form-urlencoded";
-export const GRAPHQL_RES = "application/graphql-response+json";
+const HTAB = String.fromCharCode(0x09);
+const SP = String.fromCharCode(0x20);
+const DQUOTE = String.fromCharCode(0x22);
+const REVERSE_DOLIDUS = String.fromCharCode(0x5c);
 
-export type Typ = typeof JSON | typeof URL_ENCODED | typeof GRAPHQL_RES;
-
-// https://datatracker.ietf.org/doc/html/rfc9110
-// https://httpwg.org/specs/rfc9110.html#rule.parameter
+const charSet = (fromCode: number, toCode: number): Set<string> => {
+  return Array.from({ length: toCode - fromCode + 1 }, (_, i) =>
+    String.fromCharCode(i + fromCode)
+  ).reduce((acc, char) => {
+    acc.add(char);
+    return acc;
+  }, new Set<string>());
+};
 
 /**
- *    ABNF of Content-Type header
- *  -----------------------------------------------------------------------
- *    Content-Type = media-type
+ * token = 1*tchar
+ * tchar = "!" / "#" / "$" / "%" / "&" / "'" / "*"
+ *       / "+" / "-" / "." / "^" / "_" / "`" / "|" / "~"
+ *       / DIGIT / ALPHA
+ * DIGIT = %x30-39             ; 0-9
+ * ALPHA = %x41-5A / %x61-7A   ; A-Z / a-z
  *
- *    media-type = type "/" subtype parameters
- *    type       = token
- *    subtype    = token
- *  -----------------------------------------------------------------------
- *    The type and subtype tokens are case-insensitive.
- *    token = https://datatracker.ietf.org/doc/html/rfc9110#section-5.6.2
+ * @see: https://datatracker.ietf.org/doc/html/rfc9110#section-5.6.2
+ * @see: https://datatracker.ietf.org/doc/html/rfc5234#appendix-B.1
  */
+// prettier-ignore
+const tcharSet = new Set([
+  "!", "#", "$", "%", "&", "'", "*",
+  "+", "-", ".", "^", "_", "`", "|", "~",
+  ...charSet(0x30, 0x39), // 0-9
+  ...charSet(0x41, 0x5a), // A-Z
+  ...charSet(0x61, 0x7a), // a-z
+]);
+const isTchar = (char: string): boolean => tcharSet.has(char);
 
 /**
- *    ABNF of parameters
- *  -----------------------------------------------------------------------
- *    parameters      = *( OWS ";" OWS [ parameter ] )
- *    parameter       = parameter-name "=" parameter-value
- *    parameter-name  = token
- *    parameter-value = ( token / quoted-string )
- *    OWS             = *( SP / HTAB )
- *  -----------------------------------------------------------------------
- *    SP = space, HTAB = horizontal tab
+ * parameter-value = ( token / quoted-string )
+ *
+ * quoted-string   = DQUOTE *( qdtext / quoted-pair ) DQUOTE
+ * qdtext          = HTAB / SP / %x21 / %x23-5B / %x5D-7E / obs-text
+ * quoted-pair     = "\" ( HTAB / SP / VCHAR / obs-text )
+ * obs-text        = %x80-FF
+ *
+ * VCHAR = %x21-7E
+ *
+ * So, character set used in qdtext is:
+ *
+ * qdTextCharSet = ( HTAB / SP / VCHAR / obs-text ) of quoted-pair - DQUOTE - REVERSE_DOLIDUS
+ *
+ * And diffSet is used for optimizing memory usage.
+ *
+ * diffSet = ( HTAB / SP / VCHAR / obs-text ) of quoted-pair - tcharSet
+ *
+ * @see: https://datatracker.ietf.org/doc/html/rfc9110#section-5.6.4
+ * @see: https://datatracker.ietf.org/doc/html/rfc9110#appendix-A
+ * @see: https://datatracker.ietf.org/doc/html/rfc5234#appendix-B.1
  */
+const diffSet = new Set([
+  HTAB,
+  SP,
+  ...charSet(0x21, 0x7e),
+  ...charSet(0x80, 0xff),
+]).difference(tcharSet);
 
-//    ABNF of Accept header
-//  -----------------------------------------------------------------------
-//    Accept      = #( media-range [ weight ] )
-//
-//    media-range = ( "*/*"
-//                    / ( type "/" "*" )
-//                    / ( type "/" subtype )
-//                  ) parameters
-//  -----------------------------------------------------------------------
+const isQdTextChar = (char: string): boolean => {
+  assert(char.length === 1);
+  if (char === DQUOTE) return false;
+  if (char === REVERSE_DOLIDUS) return false;
+  return tcharSet.has(char) || diffSet.has(char);
+};
+
+const canFollowBackslash = (char: string): boolean => {
+  assert(char.length === 1);
+  return tcharSet.has(char) || diffSet.has(char);
+};
+
+/**
+ * OWS = *( SP / HTAB )
+ * @see https://datatracker.ietf.org/doc/html/rfc9110#whitespace
+ */
+const isOWSChar = (char: string): boolean => {
+  return char === SP || char === HTAB;
+};
 
 const removeLeadingWhitespace = (str: string): string => {
   return str.replace(/^[ \t\n\r]+/u, "");
@@ -56,115 +96,61 @@ const removeSurroundingWhitespace = (str: string): string => {
   return removeLeadingWhitespace(removeTrailingWhitespace(str));
 };
 
-// OWS = *( SP / HTAB ) ref: https://datatracker.ietf.org/doc/html/rfc9110#whitespace
-const isOWS = (char: string): boolean => {
-  return char === " " || char === "\t" || char === "\n" || char === "\r";
-};
-
-const isToken = (str: string): boolean => {
-  // ABNF of token: https://datatracker.ietf.org/doc/html/rfc9110#section-5.6.2
-  return /^[-!#$%&'*+.^_`|~A-Za-z0-9]+$/u.test(str);
-};
-
-// const isQdText = (str: string): boolean => {
-//   // \u0022 = ", \u005C = \
-//   return /^[\t\s\u0021\u0023-\u005B\u005D-\u007E\u0080-\u00FF]+$/u.test(str);
-// };
-
 /**
- * Check if the character can follow backslash.
+ * quoted-string   = DQUOTE *( qdtext / quoted-pair ) DQUOTE
+ * qdtext          = HTAB / SP / %x21 / %x23-5B / %x5D-7E / obs-text
+ * quoted-pair     = "\" ( HTAB / SP / VCHAR / obs-text )
  *
- * [ABNF]
- * quoted-pair = "\" ( HTAB / SP / VCHAR / obs-text )
- * VCHAR       = %x21-7E
- * obs-text    = %x80-FF
+ * Get *( qdtext / quoted-pair ) of "quoted-string" from input.
  *
- * VCHAR is defined in https://datatracker.ietf.org/doc/html/rfc5234#appendix-B.1
- *
- * @param char
- * @returns boolean
+ * @returns [string, number] if success, undefined if failure. The second element is the next position of closing DQUOTE.
+ * @see https://datatracker.ietf.org/doc/html/rfc9110#section-5.6.4
  */
-// const canFollowBackslash = (char: string): boolean => {
-//   assert(char.length === 1);
-//   return /^[\t\s\u0021-\u007E\u0080-\u00FF]$/u.test(char);
-// };
-
-/**
- * Check if the string can be *( qdtext / quoted-pair ) .
- * It handles a quoted-pair as if it were replaced by the octet following the backslash.
- *
- * [ABNF]
- * quoted-string  = DQUOTE *( qdtext / quoted-pair ) DQUOTE
- *
- * @param str
- * @returns
- */
-const canBeStringInnerQuotations = (str: string): boolean => {
-  // union of qdtext and quoted-pair
-  return /^[\t\s\u0021-\u007E\u0080-\u00FF]*$/u.test(str);
-};
-
 const getStringInnerQuotations = (
   input: string,
   position: number
-): [string, number] => {
-  // [whatwg]: 1. Let positionStart be position.
-  // => positionStart is unnecessary in this implementation.
+): [string, number] | undefined => {
+  assert(input[position] === DQUOTE);
 
-  // [whatwg]: 2. Let value be the empty string.
   let value = "";
 
-  // [whatwg]: 3. Assert: input[position] is U+0022 (").
-  assert(input[position] === '"');
-
-  // [whatwg]: 4. Advance position by 1.
+  // Advance position by 1, which skips '"'.
   position++;
 
   const len = input.length;
 
-  // [whatwg]: 5. While true:
   while (true) {
-    // [whatwg]: 5-1. Append the result of collecting a sequence of code points that are not U+0022 (") or U+005C (\) from input, given position, to value.
-    while (position < len && input[position] !== '"' && input[position] !== "\\") {
+    while (position < len && isQdTextChar(input[position]!)) {
       value += input[position];
       position++;
     }
 
-    // [whatwg]: 5-2. If position is past the end of input, then break.
-    if (position >= len) break;
+    // The end of input must be '"'.
+    if (position >= len) return undefined;
 
-    // [whatwg]: 5-3. Let quoteOrBackslash be the code point at position within input.
-    const quoteOrBackslash = input[position];
-
-    // [whatwg]: 5-4. Advance position by 1.
-    position++;
-
-    // [whatwg]: 5-5. If quoteOrBackslash is U+005C (\), then:
-    if (quoteOrBackslash === "\\") {
-      // [whatwg]: 5-5-1. If position is past the end of input, then append U+005C (\) to value and break.
-      if (position >= len) {
-        value += quoteOrBackslash;
-        break;
-      }
-
-      // [whatwg]: 5-5-2. Append the code point at position within input to value.
-      value += input[position];
-
-      // [whatwg]: 5-5-3. Advance position by 1.
+    if (input[position] === DQUOTE) {
       position++;
-    } else {
-      // [whatwg]: 5-6. Otherwise:
-
-      // [whatwg]: 5-6-1. quoteOrBackslash is U+0022 (").
-      assert(quoteOrBackslash === '"');
-
-      // [whatwg]: 5-6-2. Break.
       break;
     }
+
+    if (input[position] !== REVERSE_DOLIDUS) {
+      return undefined;
+    }
+
+    // input[position] is REVERSE_DOLIDUS.
+
+    position++;
+
+    if (position >= len) {
+      return undefined;
+    }
+
+    if (!canFollowBackslash(input[position]!)) return undefined;
+
+    value += input[position];
+    position++;
   }
 
-  // [whatwg]: 6. If extract-value is true, then return value.
-  // [whatwg]: 7. Return the code points from positionStart to position, inclusive, within input.
   return [value, position];
 };
 
@@ -175,51 +161,68 @@ export interface MediaType {
   parameters: Map<string, string>;
 }
 
-// https://mimesniff.spec.whatwg.org/#parsing-a-mime-type
-export const parseMediaType = (str: string): MediaType | null => {
-  // [rfc9110]: A field value does not include leading or trailing whitespace.
-  //            When a specific version of HTTP allows such whitespace to appear in a message,
-  //            a field parsing implementation MUST exclude such whitespace prior to evaluating the field value.
-  // [whatwg] : 1. Remove any leading and trailing HTTP whitespace from input.
+/**
+ *    ABNF of Content-Type header
+ *  -----------------------------------------------------------------------
+ *    Content-Type = media-type
+ *
+ *    media-type = type "/" subtype parameters
+ *    type       = token
+ *    subtype    = token
+ *  -----------------------------------------------------------------------
+ *    The type and subtype tokens are case-insensitive.
+ *    token = https://datatracker.ietf.org/doc/html/rfc9110#section-5.6.2
+ *
+ *    ABNF of parameters
+ *  -----------------------------------------------------------------------
+ *    parameters      = *( OWS ";" OWS [ parameter ] )
+ *    parameter       = parameter-name "=" parameter-value
+ *    parameter-name  = token
+ *    parameter-value = ( token / quoted-string )
+ *    OWS             = *( SP / HTAB )
+ *  -----------------------------------------------------------------------
+ *    SP = space, HTAB = horizontal tab
+ *
+ *  @returns MediaType if success, undefined if failure. It Parses the "media-type" strictly based on specification.
+ *  @see https://datatracker.ietf.org/doc/html/rfc9110#section-8.3.1
+ */
+export const parseMediaType = (str: string): MediaType | undefined => {
+  // [rfc9110]: https://datatracker.ietf.org/doc/html/rfc9110#section-5.5
+  // A field value does not include leading or trailing whitespace.
+  // When a specific version of HTTP allows such whitespace to appear in a message,
+  // a field parsing implementation MUST exclude such whitespace prior to evaluating the field value.
   str = removeSurroundingWhitespace(str);
 
   const len = str.length;
 
-  // [whatwg]: 2. Let position be a position variable for input, initially pointing at the start of input.
   let position = 0;
 
-  // [whatwg]: 3. Let type be the result of collecting a sequence of code points that are not U+002F (/) from input, given position.
   let type = "";
-  while (position < len && str[position] !== "/") {
+  while (position < len && isTchar(str[position]!)) {
     type += str[position];
     position++;
   }
 
-  // [whatwg]: 4. If type is the empty string or does not solely contain HTTP token code points, then return failure.
-  // [whatwg]: 5. If position is past the end of input, then return failure.
-  if (type === "" || position >= len || !isToken(type)) {
-    return null;
+  if (type === "" || str[position] !== "/" || position >= len) {
+    return undefined;
   }
 
-  // [whatwg]: 6. Advance position by 1. (This skips past U+002F (/).)
+  // Advance position by 1. (This skips past U+002F (/).)
   position++;
 
-  // [whatwg]: 7. Let subtype be the result of collecting a sequence of code points that are not U+003B (;) from input, given position.
   let subtype = "";
-  while (position < len && str[position] !== ";") {
+  while (position < len && isTchar(str[position]!)) {
     subtype += str[position];
     position++;
   }
 
-  // [whatwg]: 8. Remove any trailing HTTP whitespace from subtype.
-  subtype = removeTrailingWhitespace(subtype);
-
-  // [whatwg]: 9. If subtype is the empty string or does not solely contain HTTP token code points, then return failure.
-  if (subtype === "" || !isToken(subtype)) {
-    return null;
+  if (
+    subtype === "" ||
+    (position < len && str[position] !== ";" && !isOWSChar(str[position]!))
+  ) {
+    return undefined;
   }
 
-  // [whatwg]: 10. Let mimeType be a new MIME type record whose type is type, in ASCII lowercase, and subtype is subtype, in ASCII lowercase.
   type = type.toLocaleLowerCase();
   subtype = subtype.toLocaleLowerCase();
   const mediaType = type + "/" + subtype;
@@ -231,80 +234,85 @@ export const parseMediaType = (str: string): MediaType | null => {
     parameters: new Map<string, string>(),
   };
 
-  // [whatwg]: 11. While position is not past the end of input:
   while (position < len) {
-    // [whatwg]: 11-1. Advance position by 1. (This skips past U+003B (;).)
-    position++;
-
-    // [whatwg]: 11-2. Collect a sequence of code points that are HTTP whitespace from input given position.
-    // if position < len, then str[position] is not undefined.
-    while (position < len && isOWS(str[position]!)) {
+    // skip OWS before ";"
+    while (position < len && isOWSChar(str[position]!)) {
       position++;
     }
 
-    // [whatwg]: 11-3. Let parameterName be the result of collecting a sequence of code points that are not U+003B (;) or U+003D (=) from input, given position.
+    // The end of input must not be OWS.
+    // This condition is not true because of "removeSurroundingWhitespace".
+    if (position >= len) {
+      return undefined;
+    }
+
+    if (str[position] !== ";") {
+      return undefined;
+    }
+
+    // Advance position by 1. (This skips past U+003B (;).)
+    position++;
+
+    // The end of input must not be ";".
+    if (position >= len) {
+      return undefined;
+    }
+
+    // skip OWS after ";"
+    while (position < len && isOWSChar(str[position]!)) {
+      position++;
+    }
+
     let parameterName = "";
-    while (position < len && str[position] !== ";" && str[position] !== "=") {
+    while (position < len && isTchar(str[position]!)) {
       parameterName += str[position];
       position++;
     }
 
-    // [whatwg]: 11-4. Set parameterName to parameterName, in ASCII lowercase.
-    parameterName = parameterName.toLocaleLowerCase();
-
-    // [whatwg]: 11-5. If position is not past the end of input, then:
-    if (position < len) {
-      if (str[position] === ";") {
-        continue;
-      } else if (str[position] === "=") {
-        position++;
-      }
-    } else {
-      // [whatwg]: 11-6. If position is past the end of input, then break.
-      break;
+    if (parameterName === "" || str[position] !== "=" || position >= len) {
+      return undefined;
     }
 
-    // [whatwg]: 11-7. Let parameterValue be null.
-    // REVIEW: I don't understand not empty string but null. null is inconvenient to use because of null check.
+    // Set parameterName to parameterName, in ASCII lowercase.
+    // parameterName is insensitive to ASCII case.
+    parameterName = parameterName.toLocaleLowerCase();
+
+    // [strict]: 11-7. Advance =.
+    position++;
+
+    // The end of input must not be "=".
+    if (position >= len) {
+      return undefined;
+    }
+
     let parameterValue = "";
-
-    // [whatwg]: 11-8. If the code point at position within input is U+0022 ("), then:
-    if (str[position] === '"') {
-      [parameterValue, position] = getStringInnerQuotations(str, position);
-      while (position < len && str[position] !== ";") {
-        position++;
+    // cases of quoted-string (if) and token (else)
+    if (str[position] === DQUOTE) {
+      const innerStringAndPosition = getStringInnerQuotations(str, position);
+      if (innerStringAndPosition === undefined) {
+        return undefined;
       }
+      [parameterValue, position] = innerStringAndPosition;
     } else {
-      // [whatwg]: 11-9. Otherwise:
-
-      // [whatwg]: 11-9-1. Set parameterValue to the result of collecting a sequence of code points that are not U+003B (;) from input, given position.
-      while (position < len && str[position] !== ";") {
+      while (position < len && isTchar(str[position]!)) {
         parameterValue += str[position];
         position++;
       }
-
-      // [whatwg]: 11-9-2. Remove any trailing HTTP whitespace from parameterValue.
-      parameterValue = removeTrailingWhitespace(parameterValue);
-
-      // [whatwg]: 11-9-3. If parameterValue is the empty string, then continue.
-      if (parameterValue === "") continue;
+      // token cannot be empty (quoted-string can be empty)
+      if (parameterValue === "") {
+        return undefined;
+      }
     }
 
-    // [whatwg]: 11-10. If all of the following are true
-    {
-      // [whatwg]: 11-10. parameterName is not the empty string
-      if (parameterName === "") continue;
-      // [whatwg]: 11-10. mimeType’s parameters[parameterName] does not exist
-      if (result.parameters.has(parameterName)) continue;
-      // [whatwg]: 11-10. parameterName solely contains HTTP token code points
-      if (!isToken(parameterName)) continue;
-      // [whatwg]: 11-10. parameterValue solely contains HTTP quoted-string token code points
-      if (!canBeStringInnerQuotations(parameterValue)) continue;
+    if (position < len && str[position] !== ";" && !isOWSChar(str[position]!)) {
+      return undefined;
     }
-    // [whatwg]: 11-10. then set mimeType’s parameters[parameterName] to parameterValue.
+
+    // Priority logic of the same parameterName
+    if (result.parameters.has(parameterName)) continue;
+
     result.parameters.set(parameterName, parameterValue);
   }
 
-  // [whatwg]: 12. Return mimeType.
   return result;
 };
