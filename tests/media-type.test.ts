0e72e563cf4bc58850d737ba6a8d2c28ea7cf383
Hodaka Suzuki <altitude3190@gmail.com>
2024-08-18 10:48:18 +0900

    test: media-type.test.ts

---
+++ tests/media-type.test.ts
@@ -0,0 +1,234 @@
+import { test, describe, TestContext } from "node:test";
+
+import { parseMediaType } from "../src/media-type.js";
+
+test("mediaType aa", (t: TestContext) => {
+  const mediaType = parseMediaType("application/json");
+  t.assert.strictEqual(mediaType!.mediaType, "application/json");
+});
+
+test("mediaType break line", (t: TestContext) => {
+  const mediaType = parseMediaType(
+    `applicat
+ion/json`
+  );
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("mediaType empty", (t: TestContext) => {
+  const mediaType = parseMediaType("");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("mediaType slash not exist", (t: TestContext) => {
+  const mediaType = parseMediaType("text");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("slash is not found", (t: TestContext) => {
+  const mediaType = parseMediaType("texthtml");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("type is not token", (t: TestContext) => {
+  const mediaType = parseMediaType("appl@ication/json");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("subtype is not token", (t: TestContext) => {
+  const mediaType = parseMediaType("application/js@on");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("spaces should be trimmed", (t: TestContext) => {
+  const mediaType = parseMediaType("   application/json    ");
+  t.assert.strictEqual(mediaType!.mediaType, "application/json");
+});
+
+test("upper cases should be lower", (t: TestContext) => {
+  const mediaType = parseMediaType("AppLicatioN/JSON");
+  t.assert.strictEqual(mediaType?.mediaType, "application/json");
+});
+
+test("type is not found", (t: TestContext) => {
+  const mediaType = parseMediaType("/texthtml");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("subtype is not found", (t: TestContext) => {
+  const mediaType = parseMediaType("textplain/");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("with parameter", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html  ;  a=b;   c=d");
+
+  t.assert.strictEqual(mediaType!.mediaType, "text/html");
+
+  t.assert.strictEqual(mediaType!.parameters.size, 2);
+
+  t.assert.strictEqual(mediaType!.parameters.get("a"), "b");
+
+  t.assert.strictEqual(mediaType!.parameters.get("c"), "d");
+});
+
+test("parameter is empty", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html  ;  ");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+// parameters is not parameters
+test("semicolon does not exist", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html    xxx");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("parameter-name does not exist", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html  ;  =yyy");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("parameter-name does not exist2", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html  ;  =yyy xxx=zzz");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("parameter-name does not exist3", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html  ;  xxx=yyy =zzz");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+describe("parameter-value does not exist", () => {
+  const mediaType = parseMediaType("text/html  ;  xxx=");
+  test("a", (t: TestContext) => {
+    t.assert.strictEqual(mediaType, undefined);
+  });
+  test("b", (t: TestContext) => {
+    t.assert.strictEqual(mediaType, undefined);
+  });
+});
+
+test("parameter-value does not exist2", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html  ;  xxx= yyy=zzz");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("parameter-value does not exist3", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html;xxx=; yyy=zzz");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("parameter-value does not exist4", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html  ;xxx=yyy; zzz=");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("parameter '=' does not exist5", (t: TestContext) => {
+  const mediaType = parseMediaType("text/html  ;xxx yyy");
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("type contains duplication", (t: TestContext) => {
+  const testContentType = "application/json; charset=utf-8; charset=utf-32";
+  const mediaType = parseMediaType(testContentType);
+  t.assert.strictEqual(mediaType?.parameters.get("charset"), "utf-8");
+});
+
+test("type contains @", (t: TestContext) => {
+  const testContentType = "application/yaml; charset=utf-8;   q=0.9;  xxx=p";
+  const mediaType = parseMediaType(testContentType);
+  t.assert.strictEqual(mediaType?.parameters.get("charset"), "utf-8");
+});
+
+test("charset backslash", (t: TestContext) => {
+  const testContentType = 'application/json; charset="utf\\\\-8"';
+  const mediaType = parseMediaType(testContentType);
+  t.assert.strictEqual(mediaType!.parameters.get("charset"), "utf\\-8");
+});
+
+test("charset double quote empty", (t: TestContext) => {
+  const testContentType = 'application/json; charset=""';
+  const mediaType = parseMediaType(testContentType);
+  t.assert.strictEqual(mediaType?.parameters.get("charset"), "");
+});
+
+test("charseta", (t: TestContext) => {
+  const testContentType = 'application/json; charset=aa"bs';
+  const mediaType = parseMediaType(testContentType);
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("now allow spaces", (t: TestContext) => {
+  const testContentType = "application/json; charset =aa";
+  const mediaType = parseMediaType(testContentType);
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("now allow spaces2", (t: TestContext) => {
+  const testContentType = "application/json; charset= aa";
+  const mediaType = parseMediaType(testContentType);
+  t.assert.strictEqual(mediaType, undefined);
+});
+
+test("now allow spaces2 but in quotes ok", (t: TestContext) => {
+  const testContentType = 'application/json; charset=" aa"';
+  const mediaType = parseMediaType(testContentType);
+  t.assert.strictEqual(mediaType!.parameters.get("charset"), " aa");
+});
+
+// test("mediaType Type", (t: TestContext) => {
+//   const headers = new Headers();
+//   headers.append(HEADER_KEY.CONTENT_TYPE, "application/json; charset=utf-8");
+//   headers.append(HEADER_KEY.CONTENT_TYPE, "application/yaml");
+
+//   const req = new Request("https://example.com", {
+//     headers,
+//   });
+//   const contentType = req.headers.get("CONTENT-TYPE");
+//   const mediaType = new mediaTypeType(contentType!);
+
+//   t.assert.strictEqual(mediaType.essence, "application/json");
+// });
+
+// test("mediaType Type charset", (t: TestContext) => {
+//   const headers = new Headers();
+//   headers.append(HEADER_KEY.CONTENT_TYPE, "application/json; charset=utf-8");
+//   headers.append(HEADER_KEY.CONTENT_TYPE, "application/yaml");
+
+//   const req = new Request("https://example.com", {
+//     headers,
+//   });
+//   const contentType = req.headers.get("CONTENT-TYPE");
+//   const mediaType = new mediaTypeType(contentType!);
+
+//   t.assert.strictEqual(mediaType.params.get("charset"), "application/json");
+// });
+
+// test("mediaType Type charset2", (t: TestContext) => {
+//   const headers = new Headers();
+//   headers.append(HEADER_KEY.CONTENT_TYPE, "application/json; charset=utf-8");
+//   headers.append(HEADER_KEY.CONTENT_TYPE, "application/yaml");
+
+//   const req = new Request("https://example.com", {
+//     headers,
+//   });
+//   const contentType = req.headers.get("CONTENT-TYPE");
+//   const mediaType = new mediaTypeType(contentType!);
+
+//   t.assert.strictEqual(mediaType.params.get("Charset"), undefined);
+// });
+
+// test("mediaType Type2", (t: TestContext) => {
+//   const headers = new Headers();
+//   headers.append(HEADER_KEY.CONTENT_TYPE, "application/yaml");
+//   headers.append(HEADER_KEY.CONTENT_TYPE, "application/json; charset=utf-8");
+
+//   const req = new Request("https://example.com", {
+//     headers,
+//   });
+//   const contentType = req.headers.get("CONTENT-TYPE");
+//   t.assert.throws(() => {
+//     new mediaTypeType(contentType!);
+//   });
+//   mediaTypeType.parse(contentType!);
+// });
