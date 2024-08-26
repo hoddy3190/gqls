912cce1728c6712b5122acc6a35e1c02f7283478
Hodaka Suzuki <altitude3190@gmail.com>
2024-08-26 21:15:37 +0900

    refactor: wip main.ts

---
+++ src/constant.ts
@@ -0,0 +1,37 @@
+import { MediaType, serializeMediaType } from "./media-type.js";
+import { StatusCode } from "./type.js";
+
+export const CONTENT_TYPE_KEY = "Content-Type";
+export const ACCEPT_KEY = "Accept";
+
+// @spec: S96
+export const DEFAULT_ERROR_STATUS_CODE = 400;
+
+export const getStatusText = (statusCode: StatusCode): string => {
+  switch (statusCode) {
+    case 200:
+      return "OK";
+    case 400:
+      return "Bad Request";
+    case 405:
+      return "Method Not Allowed";
+    case 406:
+      return "Not Acceptable";
+    default:
+      const _exhaustiveCheck: never = statusCode;
+      return _exhaustiveCheck;
+  }
+};
+
+// @spec: S18
+export const DEFAULT_ENCODING = "utf-8";
+
+export const GQL_RESPONSE_MEDIA_TYPE: MediaType = {
+  type: "application",
+  subtype: "graphql-response+json",
+  mediaType: "application/graphql-response+json",
+  parameters: { charset: DEFAULT_ENCODING },
+};
+export const GQL_RESPONSE_CONTENT_TYPE = serializeMediaType(
+  GQL_RESPONSE_MEDIA_TYPE
+);
--- src/main.ts
+++ src/main.ts
@@ -1,231 +1,375 @@
-import { execute, specifiedRules, validate } from "graphql";
-
-import * as HEADER_KEY from "./header.js";
-import { includeMediaType, MediaType, parseMediaRange, parseMediaType } from "./media-type.js";
-import { parse } from "path/posix";
 import { assert } from "node:console";
-import { GqlRequestError, isGqlRequestError, isRequestError, MaybeGqlRequestError, RequestError, Result, Success } from "./type.js";
+import {
+  ACCEPT_KEY,
+  CONTENT_TYPE_KEY,
+  DEFAULT_ERROR_STATUS_CODE,
+  getStatusText,
+  GQL_RESPONSE_MEDIA_TYPE,
+} from "./constant.js";
+import {
+  includeMediaType,
+  parseMediaRange,
+  parseMediaType,
+} from "./media-type.js";
+import {
+  GqlImpl,
+  GqlRequestErrorResponseAndHttpStatus,
+  GqlResponseAndHttpStatus,
+  GqlRequest,
+  GqlResponse,
+  isGqlSuccessOrPartialSuccess,
+  MaybeGqlRequestError,
+  isGqlRequestErrorResponseAndHttpStatus,
+  StatusCode,
+} from "./type.js";
 
-// S18
-const DEFAULT_ENCODING = "utf-8";
-
-// @spec: S96
-const DEFAULT_ERROR_STATUS_CODE = 400;
-
-export type StatusCode = 200 | typeof DEFAULT_ERROR_STATUS_CODE | 405 | 406;
-
-
-
-
-
-
-
-const GET_REQ_MEDIA_TYPE  = "application/x-www-form-urlencoded";
+const GET_REQ_MEDIA_TYPE = "application/x-www-form-urlencoded";
 const POST_REQ_MEDIA_TYPE = "application/json";
-const RES_MEDIA_TYPE: MediaType = {
-    type: "application",
-    subtype: "graphql-response+json",
-    mediaType: "application/graphql-response+json",
-}
-const RES_CONTENT_TYPE = RES_MEDIA_TYPE.mediaType + "; charset=" + DEFAULT_ENCODING;
 
-
-const RECOGNIZED_MEDIA_TYPE = {
-    // S14
-    REQ: new Set<MIME_TYPE.Typ>({
-        MIME_TYPE.JSON,
-    }),
-    // S15, S16
-    RES:  new Set<MIME_TYPE.Typ>({
-        MIME_TYPE.JSON,
-        MIME_TYPE.GRAPHQL_RES,
-    }),
-};
+// const RECOGNIZED_MEDIA_TYPE = {
+//     // S14
+//     REQ: new Set<MIME_TYPE.Typ>({
+//         MIME_TYPE.JSON,
+//     }),
+//     // S15, S16
+//     RES:  new Set<MIME_TYPE.Typ>({
+//         MIME_TYPE.JSON,
+//         MIME_TYPE.GRAPHQL_RES,
+//     }),
+// };
 
 // S19
 type ServerAcceptableMethods =
-    "POST" // MUST
+  | "POST" // MUST
   | "GET"; // MAY
 
 const SERVER_MUST_SUPPORT_MIME_TYPES = new Set([
-    // S80, S11, S14
-    "application/json",
-    // S81
-    "application/graphql-response+json",
+  // S80, S11, S14
+  "application/json",
+  // S81
+  "application/graphql-response+json",
 ]);
 
-const isWellFormedGraphQLRequest = (req: Request): boolean => {
-    return false;
+const buildSimpleGqlRequestErrorResponse = (
+  statusCode: StatusCode = DEFAULT_ERROR_STATUS_CODE
+): GqlRequestErrorResponseAndHttpStatus => {
+  return {
+    httpResult: { statusCode, message: null },
+    gqlResponse: {
+      errors: [
+        {
+          message: getStatusText(statusCode),
+          locations: [],
+        },
+      ],
+      extensions: {},
+    },
+  };
 };
-
-
-export const validateContentType = (contentType: string): boolean => {
-    try {
-        const mimeType = new MIMEType(contentType);
-        const charset = mimeType.params.get("charset");
-        if (charset) {
-            if (charset !== DEFAULT_ENCODING) return false;
-        }
-        if (mimeType.essence != MIME_TYPE.JSON) {
-            return false;
-        };
-    } catch (e) {
-        return false;
-    }
-
-    return true;
-}
-
-const statusCodeToStatusText: { [code in StatusCode]: string } = {
-    200: "OK",
-    400: "Bad Request",
-    405: "Method Not Allowed",
-    406: "Not Acceptable",
-};
-
-const makeErrorResponse = (statusCode: StatusCode, bodyText?: string): Response => {
-    const statusText = statusCodeToStatusText[statusCode];
-    return new Response(bodyText, { status: statusCode, statusText });
-}
-
-interface WellFormedGqlRequest {
-    query: string;
-    operationName?: string;
-    variables?: Record<string, unknown>;
-    extensions?: Record<string, unknown>;
-}
 
 const isStringRecord = (o: unknown): o is Record<string, unknown> => {
-    return (
-        !!o && typeof o === 'object' && !Buffer.isBuffer(o) && !Array.isArray(o)
-    );
-}
+  return !!o && typeof o === "object" && !Buffer.isBuffer(o) && !Array.isArray(o);
+};
 
 const isNonEmptyStringRecord = (o: unknown): o is Record<string, unknown> => {
-    return isStringRecord(o) && Object.keys(o).length > 0;
-}
+  return isStringRecord(o) && Object.keys(o).length > 0;
+};
 
-const isWellFormedGqlRequest = (data: unknown): data is WellFormedGqlRequest => {
-    if (!isStringRecord(data)) return false;
+const isGqlRequest = (data: unknown): data is GqlRequest => {
+  if (!isStringRecord(data)) return false;
 
-    const len = Object.keys(data).length;
-    // The key "query" is required, and the keys "operationName", "variables", and "extensions" are optional.
-    // At most these 4 keys are allowed.
-    if (len < 1 || len > 4) return false;
+  const len = Object.keys(data).length;
+  // The key "query" is required, and the keys "operationName", "variables", and "extensions" are optional.
+  // At most these 4 keys are allowed.
+  if (len < 1 || len > 4) return false;
 
-    let keyCount = 0;
+  let keyCount = 0;
 
-    // @spec: S21, S65
-    if (!("query" in data) || typeof data["query"] !== "string") return false;
+  // @spec: S21, S65
+  if (!("query" in data) || typeof data["query"] !== "string") return false;
+  keyCount++;
+
+  // @spec: S22, S66
+  if ("operationName" in data) {
+    if (typeof data["operationName"] !== "string") return false;
     keyCount++;
+  }
 
-    // @spec: S22, S66
-    if ("operationName" in data) {
-        if (typeof data["operationName"] !== "string") return false;
-        keyCount++;
-    }
+  // @spec: S23, S67
+  if ("variables" in data) {
+    if (!isStringRecord(data["variables"])) return false;
+    keyCount++;
+  }
 
-    // @spec: S23, S67
-    if ("variables" in data) {
-        if (!isStringRecord(data["variables"])) return false;
-        keyCount++;
-    }
+  // @spec: S24, S68
+  if ("extensions" in data) {
+    if (!isStringRecord(data["extensions"])) return false;
+    keyCount++;
+  }
 
-    // @spec: S24, S68
-    if ("extensions" in data) {
-        if (!isStringRecord(data["extensions"])) return false;
-        keyCount++;
-    }
+  // @spec: S69
+  if (keyCount !== len) return false;
 
-    // @spec: S69
-    if (keyCount !== len) return false;
+  return true;
+};
 
-    return true;
-}
+export const validatePostRequestHeaders = (
+  headers: Request["headers"]
+): GqlRequestErrorResponseAndHttpStatus | null => {
+  // @spec: S26, S88, S25
+  // S88 mentions that a request that doesn't contain Accept header should be treated as if it had Accept: application/graphql-response+json,
+  // but S26 says the server may respond with an error against such a request.
+  // In this implementation, we treat such a request as an error because of simplicity.
+  const clientAcceptableMediaType = headers.get(ACCEPT_KEY);
+  if (!clientAcceptableMediaType) {
+    // S5: 4xx or 5xx status code
+    return buildSimpleGqlRequestErrorResponse();
+  }
 
-export const makeGqlReq = (data: Request): MaybeGqlRequestError<WellFormedGqlRequest> => {
-    // if (data.method) {
+  // S36, S37
+  // S35を上書き
+  const mediaRange = parseMediaRange(clientAcceptableMediaType);
+  if (!mediaRange) {
+    // S5, S95, S97, S98
+    return buildSimpleGqlRequestErrorResponse();
+  }
+  // @spec: S35, S36, S37, S76, S78, S79, S81, S86
+  // We don't check whether the Accept header contains application/json because of S37.
+  // application/json is no longer required after the watershed.
+  // We support application/graphql-response+json only. <-> TODO: 矛盾 S80
+  if (!includeMediaType(mediaRange, GQL_RESPONSE_MEDIA_TYPE)) {
+    // @spec: S5, S95, S97, S98, S77
+    return buildSimpleGqlRequestErrorResponse(406);
+  }
 
-    // } else if (data.method) {
+  return null;
+};
 
-    // }
-    // else {
+export const buildGqlRequestFromPost = async (
+  httpRequest: Request
+): Promise<MaybeGqlRequestError<GqlRequest>> => {
+  assert(httpRequest.method === "POST");
 
-    // }
-    return { data: {} as WellFormedGqlRequest };
-}
+  const validationResult = validatePostRequestHeaders(httpRequest.headers);
+  if (validationResult !== null) {
+    return validationResult;
+  }
 
-export const makeGqlReqFromGetReq = (req: Request): Result<WellFormedGqlRequest> => {};
+  let body: unknown;
+  try {
+    body = await httpRequest.json();
+  } catch (e) {
+    return buildSimpleGqlRequestErrorResponse(400);
+  }
 
+  // TODO: need null check?
+  if (typeof body !== "object") {
+    return buildSimpleGqlRequestErrorResponse(400);
+  }
 
-export const makeGqlReqFromPostReq = (req: Request): Result<WellFormedGqlRequest> => {
+  if (!isGqlRequest(body)) {
+    return buildSimpleGqlRequestErrorResponse(400);
+  }
+  return { data: body };
+};
 
-}
+export const validateGetRequestHeaders = (
+  headers: Request["headers"]
+): GqlRequestErrorResponseAndHttpStatus | null => {
+  const clientAcceptableMediaType = headers.get(ACCEPT_KEY);
+  // @spec: S26, S88, S25
+  // S88 mentions that a request that doesn't contain Accept header should be treated as if it had Accept: application/graphql-response+json,
+  // but S26 says the server may respond with an error against such a request.
+  // In this implementation, we treat such a request as an error because of simplicity.
+  if (!clientAcceptableMediaType) {
+    // S5: 4xx or 5xx status code
+    return buildSimpleGqlRequestErrorResponse();
+  }
 
-export const getDocument = (req: Request): Result<WellFormedGqlRequest> => {
-    const wellFormedGqlRequest = makeGqlReq(req);
-    if (isGqlRequestError(wellFormedGqlRequest)) {
-        return wellFormedGqlRequest;
-    }
+  // S36, S37
+  // S35を上書き
+  const mediaRange = parseMediaRange(clientAcceptableMediaType);
+  if (!mediaRange) {
+    // S5, S95, S97, S98
+    return buildSimpleGqlRequestErrorResponse();
+  }
+  // @spec: S35, S36, S37, S76, S78, S79, S81, S86
+  // We don't check whether the Accept header contains application/json because of S37.
+  // application/json is no longer required after the watershed.
+  // We support application/graphql-response+json only. <-> TODO: 矛盾 S80
+  if (!includeMediaType(mediaRange, GQL_RESPONSE_MEDIA_TYPE)) {
+    // @spec: S5, S95, S97, S98, S77
+    return buildSimpleGqlRequestErrorResponse(406);
+  }
 
-    let document;
+  const contentType = headers.get(CONTENT_TYPE_KEY);
+  if (!contentType) {
+    // S5, S95, S97, S98
+    return buildSimpleGqlRequestErrorResponse();
+  }
+  const parsedContentType = parseMediaType(contentType);
+  if (parsedContentType === undefined) {
+    // S5, S95, S97, S98
+    return buildSimpleGqlRequestErrorResponse();
+  }
+  // @spec: S40
+  // It is not explicitly written that the Content-Type header is required for GET requests or Content-Type header should be "application/x-www-form-urlencoded".
+  // We interpret that S40 says the Content-Type header is required and should be "application/x-www-form-urlencoded".
+  if (parsedContentType.mediaType !== GET_REQ_MEDIA_TYPE) {
+    // @spec: S5, S95, S97, S98
+    return buildSimpleGqlRequestErrorResponse();
+  }
+
+  return null;
+};
+
+export const buildGqlRequestFromGet = (
+  httpRequest: Request
+): MaybeGqlRequestError<GqlRequest> => {
+  assert(httpRequest.method === "GET");
+
+  const validationResult = validateGetRequestHeaders(httpRequest.headers);
+  if (validationResult !== null) {
+    return buildSimpleGqlRequestErrorResponse();
+  }
+
+  if (!URL.canParse(httpRequest.url)) {
+    // S5, S95, S97, S98
+    return buildSimpleGqlRequestErrorResponse();
+  }
+  // @spec: S45
+  const searchParams = new URL(httpRequest.url).searchParams;
+
+  const query = searchParams.get("query");
+  // @spec: S21
+  if (query === null) {
+    // @spec: S5, S95, S97, S98, S115, S109
+    // S115 examples POST requests, but as the interpretation of S115, it is also applicable to GET requests.
+    return buildSimpleGqlRequestErrorResponse();
+  }
+
+  // @spec: S41, S50, S51, S52
+  // S41: "query" is string type (not null type) because it passes null check.
+  //      "startWith" method also checks if the query value is empty or not.
+  // TODO: Queryは許可する？
+  if (!query.startsWith("query")) {
+    return buildSimpleGqlRequestErrorResponse(405);
+  }
+
+  // @spec: S22, S42, S44, S46
+  // If the operationName parameter is present, it is string. If not, it is null.
+  // S44 doesn't affect the implementation.
+  // If searchParams.get("operationName") is an empty string, null assigns operationNameParam because of S46.
+  const operationNameStr = searchParams.get("operationName");
+  const operationName = operationNameStr !== "" ? operationNameStr : null;
+
+  // @spec: S23, S43
+  let variables = {};
+  const variablesStr = searchParams.get("variables");
+  // TODO: permit empty string?
+  if (variablesStr) {
     try {
-        document = parse(wellFormedGqlRequest.data.query);
+      variables = JSON.parse(variablesStr);
     } catch (e) {
-        return { errors: [] };
+      // @spec: S5, S95, S97, S98, S43, S109
+      return buildSimpleGqlRequestErrorResponse();
     }
+  }
 
-    const validationErrors = validate(schema, document, specifiedRules);
-    if (validationErrors.length > 0) {
-        // @spec: S94, S109, S117
-        // TODO: cache
-        return { errors: [] };
-    }
-
-    return { data: {
-        ...wellFormedGqlRequest,
-        document
-    }}
-}
-
-export const getDocument = (document: string): MaybeGqlRequestError<{document: string}> => {
-    let document;
+  // @spec: S24, S43
+  let extensions = {};
+  const extensionsStr = searchParams.get("extensions");
+  // TODO: permit empty string?
+  if (extensionsStr) {
     try {
-        document = parse(wellFormedReq.query);
+      extensions = JSON.parse(extensionsStr);
     } catch (e) {
-        return { errors: [] };
+      // @spec: S5, S95, S97, S98, S43, S109
+      return buildSimpleGqlRequestErrorResponse();
     }
+  }
 
-    const validationErrors = validate(schema, document, specifiedRules);
-    if (validationErrors.length > 0) {
-        // @spec: S94, S109, S117
-    }
-    return document;
-}
+  const gqlRequest = { query, operationName, variables, extensions };
+  if (!isGqlRequest(gqlRequest)) {
+    return buildSimpleGqlRequestErrorResponse();
+  }
 
-export const validate = (schema, document, specifiedRules): GqlRequestError | null => {
-    const validationErrors = validate(schema, document, specifiedRules);
-    if (validationErrors.length > 0) {
-        // @spec: S94, S109, S117
-    }
-    return null;
-}
+  return {
+    data: gqlRequest,
+  };
+};
 
+export const buildGqlRequest = async (
+  httpRequest: Request
+): Promise<MaybeGqlRequestError<GqlRequest>> => {
+  if (httpRequest.method === "POST") {
+    return await buildGqlRequestFromPost(httpRequest);
+  } else if (httpRequest.method === "GET") {
+    return buildGqlRequestFromGet(httpRequest);
+  }
+  return buildSimpleGqlRequestErrorResponse(405);
+};
 
+// export const getDocument = (req: Request): Result<GqlRequest> => {
+//     const GqlRequest = buildGqlRequest(req);
+//     if (isGqlRequestError(GqlRequest)) {
+//         return GqlRequest;
+//     }
 
-export const execute = (req: Request): MaybeGqlFieldErrorResult<string> => {
+//     let document;
+//     try {
+//         document = parse(GqlRequest.data.query);
+//     } catch (e) {
+//         return { errors: [] };
+//     }
 
+//     const validationErrors = validate(schema, document, specifiedRules);
+//     if (validationErrors.length > 0) {
+//         // @spec: S94, S109, S117
+//         // TODO: cache
+//         return { errors: [] };
+//     }
 
-}
+//     return { data: {
+//         ...GqlRequest,
+//         document
+//     }}
+// }
+
+// export const getDocument = (document: string): MaybeGqlRequestError<{document: string}> => {
+//     let document;
+//     try {
+//         document = parse(wellFormedReq.query);
+//     } catch (e) {
+//         return { errors: [] };
+//     }
+
+//     const validationErrors = validate(schema, document, specifiedRules);
+//     if (validationErrors.length > 0) {
+//         // @spec: S94, S109, S117
+//     }
+//     return document;
+// }
+
+// export const validate = (schema, document, specifiedRules): GqlRequestError | null => {
+//     const validationErrors = validate(schema, document, specifiedRules);
+//     if (validationErrors.length > 0) {
+//         // @spec: S94, S109, S117
+//     }
+//     return null;
+// }
+
+/*
+
 
 // バリデーションからレスポンスまで
 export const processGql = (req: Request): GqlOverHttpResult<string> => {
-    const wellFormedGqlRequestResult = makeGqlReq(req);
-    if (isGqlRequestError(wellFormedGqlRequestResult)) {
-        return wellFormedGqlRequestResult;
+    const GqlRequestResult = buildGqlRequest(req);
+    if (isGqlRequestError(GqlRequestResult)) {
+        return GqlRequestResult;
     }
 
-    const documentResult = getDocument(wellFormedGqlRequestResult.data.query);
+    const documentResult = getDocument(GqlRequestResult.data.query);
     if (isRequestError(documentResult)) {
         return documentResult;
     }
@@ -274,7 +418,7 @@ export const handle = (req: Request): Response => {
     // In this implementation, we treat such a request as an error because of simplicity.
     if (!clientAcceptableMimeType) {
         // S5: 4xx or 5xx status code
-        return new Response("Bad request", { status: 4xx });
+        return buildSimpleGqlRequestErrorResponse();
     }
 
     // S36, S37
@@ -282,7 +426,7 @@ export const handle = (req: Request): Response => {
     const mediaRange = parseMediaRange(clientAcceptableMimeType);
     if (!mediaRange) {
         // S5, S95, S97, S98
-        return new Response("Bad request", { status: 4xx });
+        return buildSimpleGqlRequestErrorResponse();
     }
     // @spec: S35, S36, S37, S76, S78, S79, S81, S86
     // We don't check whether the Accept header contains application/json because of S37.
@@ -354,7 +498,7 @@ export const handle = (req: Request): Response => {
             return new Response("Bad request", { status: 400 });
         }
 
-        if (!isWellFormedGqlRequest(body)) {
+        if (!isGqlRequest(body)) {
             // @spec: S5, S95, S97, S98, S109, S115
             return new Response("Bad request", { status: 400 });
         }
@@ -430,7 +574,7 @@ export const handle = (req: Request): Response => {
 
 
     // @spec: S90
-    // assert(data is WellFormedGqlRequest);
+    // assert(data is GqlRequest);
 
     // TODO: @spec: S71, S72, S73
     // 以降は、wellformedgqlresponse を返さないといけない！
@@ -483,3 +627,31 @@ export const handle = (req: Request): Response => {
         }
     });
 }
+*/
+
+export const buildGqlOverHttpResult = <T>(
+  gqlResponse: GqlResponse<T>
+): GqlResponseAndHttpStatus<T> => {
+  if (isGqlSuccessOrPartialSuccess(gqlResponse)) {
+    return {
+      httpResult: { statusCode: 200, message: null },
+      gqlResponse,
+    };
+  }
+  return {
+    httpResult: { statusCode: 400, message: null },
+    gqlResponse,
+  };
+};
+
+export const handle = async <T>(
+  httpRequest: Request,
+  gqlImpl: GqlImpl<T>
+): Promise<GqlResponseAndHttpStatus<T>> => {
+  const GqlRequest = await buildGqlRequest(httpRequest);
+  if (isGqlRequestErrorResponseAndHttpStatus(GqlRequest)) {
+    return GqlRequest;
+  }
+  const gqlResponse = await gqlImpl(GqlRequest.data);
+  return buildGqlOverHttpResult(gqlResponse);
+};
